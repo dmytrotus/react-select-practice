c722bd79f2277607dc842010ac0a1c69a5f804ad
Dima <dmytrotus@gmail.com>
2021-01-08 17:06:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Select from 'react-select';
+import { colourOptions } from './data';
+
+function App(){
+
+  const innerRef = useRef(null);
+
+  const makeFocus = () => {
+    innerRef.current.focus();
+  }
+
+  const disableFocus = () => {
+    innerRef.current.blur();
+  }
+
+  const[placeholder, setPlaceholder] = useState('Wybierz typ')
+  const handleMenuOpen = () => {
+    console.log('open')
+    setPlaceholder('Zacznij pisać...')
+    console.log(innerRef.current)
+    //innerRef.current.blur();
+  }
+  const handleMenuClose = () => {
+    console.log('Close')
+    setPlaceholder('Wybierz typ')
+    console.log(innerRef.current)
+    //innerRef.current.blur();
+  }
+
+  return(
+    <Fragment>
+    <Select
+    placeholder={placeholder}
+    ref={innerRef} 
+    options={colourOptions}
+    onMenuOpen={handleMenuOpen}
+    onMenuClose={handleMenuClose}
+    />
+    <button onClick={makeFocus}>Focus</button>
+    <button onClick={disableFocus}>Disable Focus</button>
+    </Fragment>
+    )
 }
 
 export default App;
